1f1eae5276525ac86c529842ad1d3a8e732e6189
GanahaHibiki <toshilang@hotmail.com>
2017-06-05 22:13:40 +0800

    update effectClass.js: constructor, getters and setters

--- classes/effectClass.js
+++ classes/effectClass.js
@@ -15,10 +15,103 @@ module.exports = Effect;
  * 
  */
 Effect = function (mParams) {
-    this.type = mParams.type;
-    this.value = mParams.value;
-    this.target = mParams.target;
-    this.targetAmount = mParams.targetAmount;
-    this.duration = mParams.duration;
-    this.times = mParams.times;
+    if (typeof(mParams) !== "object") {
+        throw new TypeError("Illeagal constructor parameter for class 'Effect'!");
+    };
+    this.setType(mParams.type);
+    this.setValue(mParams.value);
+    this.setTarget(mParams.target);
+    this.setTargetAmount(mParam.targetAmount);
+    this.setDuration(mParam.duration);
+    this.setTimes(mParam.times);
+
+    return this;
+}
+
+// getter and setter for property 'type'
+Effect.prototype.getType = function () {  
+    return this.type;
+}
+Effect.prototype.setType = function (sType) {  
+    if (typeof(sType) !== "string") {
+        throw new TypeError("Illeagal input type when setting Effect.value!");
+    }
+    this.type = sType;
+
+    return this;
+}
+
+// getter and setter for property 'value'
+Effect.prototype.getValue = function () {  
+    return this.value;
+}
+Effect.prototype.setValue = function (fValue) {  
+    if (isNaN(fValue)) {
+        throw new TypeError("Illeagal input type when setting Effect.value!");
+    }
+    this.value = fValue;
+
+    return this;
+}
+
+// getter and setter for property 'target'
+Effect.prototype.getTarget = function () {  
+    return this.target;
+}
+Effect.prototype.setTarget = function (sTarget) {  
+    if (typeof(sTarget) !== "string") {
+        throw new TypeError("Illeagal input type when setting Effect.target!");
+    }
+    this.target = sTarget;
+
+    return this;
+}
+
+// getter and setter for property 'targetAmount'
+Effect.prototype.getTargetAmount = function () {  
+    return this.targetAmount;
+}
+Effect.prototype.setTargetAmount = function (iTargetAmount) {  
+    if (isNaN(iTargetAmount)) {
+        throw new TypeError("Illeagal input type when setting Effect.targetAmount!");
+    }
+    this.targetAmount = iTargetAmount;
+
+    return this;
+}
+
+// getter and setter for property 'duration'
+Effect.prototype.getDuration = function () {  
+    return this.duration;
+}
+Effect.prototype.setDuration = function (fDuration) {  
+    if (isNaN(fDuration)) {
+        throw new TypeError("Illeagal input type when setting Effect.duration!");
+    }
+    this.duration = fDuration;
+
+    return this;
+}
+
+// getter and setter for property 'times'
+Effect.prototype.getTimes = function () {  
+    return this.times;
+}
+Effect.prototype.setTimes = function (iTimes) {  
+    if (isNaN(iTimes)) {
+        throw new TypeError("Illeagal input type when setting Effect.vatimeslue!");
+    }
+    this.times = iTimes;
+
+    return this;
+}
+
+// Override toString function
+Effect.prototype.toString = function () {
+    return "Effect: {'type': " + this.getType() + ", " 
+                + "'value': " + this.getValue() + ", "
+                + "'target': " + this.getTarget() + ", "
+                + "'targetAmount': " + this.getTargetAmount() + ", "
+                + "'duration': " + this.getDuration() + ", "
+                + "'times': " + this.getTimes() + "} ";
 }
