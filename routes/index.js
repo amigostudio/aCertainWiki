2f38ed3dc3f16f62cd67858bdf746b9d84794099
Chuning Song <chuning.song@sap.com>
2017-06-08 07:57:22 -0700

    Revising the homepage & deploying to heroku

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '某韩国手游的WIKI站' });
 });
 
 module.exports = router;
